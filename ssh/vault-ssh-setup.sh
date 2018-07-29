f9a78c109942fa86c85450aa6b913a2e5cbd98f9
DS063137 <dave.swersky@cerner.com>
2018-07-29 16:45:25 -0500

    Added helper comments to sshd, and long commands to mariadb/setup_commands.txt

--- ssh/vault-ssh-setup.sh
+++ ssh/vault-ssh-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#TEST: vault-ssh-helper -dev -verify-only -config=/etc/vault-ssh-helper.d/config.hcl
 export VAULT_ADDR=http://vaultserver:8200
 wget https://releases.hashicorp.com/vault-ssh-helper/0.1.4/vault-ssh-helper_0.1.4_linux_amd64.zip
 unzip vault-ssh-helper_0.1.4_linux_amd64.zip
